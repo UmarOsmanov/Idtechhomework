9393581676e4e5e22373628a55b764376644d1b9
UmarOsmanov <umarosmanov98@gmail.com>
2025-01-27 14:36:42 +0400

    DONE

--- Lab6/main.js
+++ Lab6/main.js
@@ -14,8 +14,4 @@ x.addEventListener("click" , ()=>{
     menu.classList.remove("active")
     })
 
-    let search = document.querySelector(".search")
-    search.addEventListener("click" , ()=>{
-        menu.classList.add("active")
-        })
 
